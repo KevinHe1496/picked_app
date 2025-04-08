7d131e9393a90ad67bdb6cec8491d3e893f4c484
Kevin Heredia <kevin@ravecodesolutions.com>
2025-04-08 09:07:52 -0500

    Initial Commit

---
+++ PickedApp/PickedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickedAppApp.swift
+//  PickedApp
+//
+//  Created by Kevin Heredia on 8/4/25.
+//
+
+import SwiftUI
+
+@main
+struct PickedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
